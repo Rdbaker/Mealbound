d3ed2664fd425bfa47a6d53dbc0bde05bc32bbd3
Ryan Baker <ryan.da.baker@gmail.com>
2017-05-22 17:37:14 -0400

    Adds the list view for locations.

--- ceraon/public/locations/views.py
+++ ceraon/public/locations/views.py
@@ -12,8 +12,44 @@ blueprint = Blueprint('location', __name__, url_prefix='/location', static_folde
 
 @blueprint.route('/', methods=['GET', 'POST'])
 @login_required
+def list():
+    """List the locations near the user."""
+    page = Location.query.paginate(page=int(request.args.get('page', 1)),
+                                   per_page=int(request.args.get('per_page',
+                                                                 10)))
+    meta_pagination = {
+        'current': {
+            'num': page.page,
+            'link': 'javascript:void(0)'
+        },
+        'first': {
+            'num': 1,
+            'link': request.path + '?page={page}&per_page={per_page}'.format(
+                page=1, per_page=page.per_page)
+        },
+        'next': {
+            'num': page.next_num or page.pages,
+            'link': request.path + '?page={page}&per_page={per_page}'.format(
+                page=page.next_num or page.pages, per_page=page.per_page)
+        },
+        'last': {
+            'num': page.pages,
+            'link': request.path + '?page={page}&per_page={per_page}'.format(
+                page=page.pages, per_page=page.per_page)
+        },
+        'prev': {
+            'num': page.prev_num or 1,
+            'link': request.path + '?page={page}&per_page={per_page}'.format(
+                page=page.prev_num or 1, per_page=page.per_page)
+        }
+    }
+
+    return render_template('public/locations/list.html', locations=page.items,
+                           pagination=meta_pagination)
+
+@blueprint.route('/new', methods=['GET', 'POST'])
+@login_required
 def create():
-    """List members."""
     form = CreateLocationForm(request.form)
     if form.validate_on_submit():
         Location.create(host=current_user, name=form.name.data)
