417b4eb33059da0bc672e8c97c791a590ca02cc5
Chris Botaish <chbota.dev@icloud.com>
2017-05-29 11:56:50 -0400

    Change users views to use Meal.is_upcoming for filtering

--- ceraon/user/views.py
+++ ceraon/user/views.py
@@ -6,15 +6,9 @@ from flask_login import login_required, current_user
 from ceraon.locations.forms import LocationForm
 from ceraon.models.locations import Location
 from ceraon.models.meals import Meal, UserMeal
-from ceraon.utils import flash_errors
-
-import datetime as dt
 
 blueprint = Blueprint('user', __name__, url_prefix='/users',
                       static_folder='../static')
-
-def filterMealByDate(meal):
-    return meal.scheduled_for >= dt.datetime.now()
 
 @blueprint.route('/me', methods=['GET'])
 @login_required
@@ -22,5 +16,5 @@ def me():
     userMeals = UserMeal.query.filter(UserMeal.user_id == current_user.id)
     myMeals = [userMeal.meal for userMeal in userMeals]
 
-    return render_template('users/me.html', meals=list(filter(filterMealByDate, myMeals)))
+    return render_template('users/me.html', meals=list(filter(lambda m: m.is_upcoming(), myMeals)))
 
