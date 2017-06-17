7391a274d66e12094a12223e86d58377a70ce261
Chris Botaish <chbota.dev@icloud.com>
2017-06-17 13:27:26 -0400

    Hook up ceraon app to ceraon store

--- CeraonUI/src/Apps/Ceraon.tsx
+++ CeraonUI/src/Apps/Ceraon.tsx
@@ -1,16 +1,28 @@
 import * as React from 'react';
-import {Button} from 'semantic-ui-react';
 
-export default class Ceraon extends React.Component<{}, {}> {
+import CeraonStore from '../Store/CeraonStore';
+import CeraonState from '../State/CeraonState';
+import NavigationBar from '../Components/NavigationBar';
+import UserSessionProvider from '../Components/UserSessionProvider';
+
+export default class Ceraon extends React.Component<{}, CeraonState> {
 	constructor() {
 		super();
+
+		this.onStoreUpdate = this.onStoreUpdate.bind(this);
+		this.state = CeraonStore.getState();
+		CeraonStore.subscribe(this.onStoreUpdate);
+	}
+
+	onStoreUpdate() {
+		this.setState(CeraonStore.getState());
 	}
 
 	render() {
 		return (
-			<Button>
-				Test
-			</Button>
+			<UserSessionProvider userSessionInfo={this.state.userSessionInfo}>
+				<NavigationBar {...this.state.navigationBarState}/>
+			</UserSessionProvider>
 		)
 	}
 
