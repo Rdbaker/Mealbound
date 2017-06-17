83b84580f91e6d17b9568d3eef3f19b17372519f
Chris Botaish <chbota.dev@icloud.com>
2017-06-17 13:26:58 -0400

    Add NavigationBar

---
+++ CeraonUI/src/Components/NavigationBar.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import {Menu, Input} from 'semantic-ui-react';
+import UserSessionInfo from '../State/Identity/UserSessionInfo';
+import UserSessionAware, {UserSessionAwareProps} from './UserSessionAware';
+import NavigationBarState from '../State/NavigationBarState';
+
+export interface NavigationBarProps extends NavigationBarState, UserSessionAwareProps<NavigationBar> {
+
+}
+
+class NavigationBar extends React.Component<NavigationBarProps, any> {
+	constructor() {
+		super();
+	}
+
+	render() {
+		return (
+			<Menu>
+				{this.props.children}
+				<Menu.Item header>Ceraon</Menu.Item>
+				<Menu.Item>
+					<Input className="icon" icon="search" placeholder="Search..."/>
+				</Menu.Item>
+				<Menu.Item position="right">
+					<Menu.Item>Create Account</Menu.Item>
+				</Menu.Item>
+			</Menu>
+		)
+	}
+}
+
+export default UserSessionAware<NavigationBar, NavigationBarProps, any>(NavigationBar);
