b7b720b502627ad149bb866358d2356134c3f541
Chris Botaish <chbota.dev@icloud.com>
2017-06-17 11:49:18 -0400

    Add first few components for UI refactor

---
+++ CeraonUI/src/Apps/Ceraon.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import {Button} from 'semantic-ui-react';
+
+export default class Ceraon extends React.Component<{}, {}> {
+	constructor() {
+		super();
+	}
+
+	render() {
+		return (
+			<Button>
+				Test
+			</Button>
+		)
+	}
+
+}
---
+++ CeraonUI/src/Main.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Ceraon from './Apps/Ceraon';
+
+ReactDOM.render(<Ceraon/>, document.getElementById('react-root'));
