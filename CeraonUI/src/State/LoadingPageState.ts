a2f5b54f96b3f6ff5167907785e3e0a0b69a1e13
Chris Botaish <chbota.dev@icloud.com>
2017-06-18 01:10:22 -0400

    Add loading page state

---
+++ CeraonUI/src/State/LoadingPageState.ts
@@ -0,0 +1,9 @@
+interface LoadingPageState {
+  loadingStatusMessage: string;
+}
+
+export const DEFAULT_LOADING_PAGE_STATE: LoadingPageState = {
+  loadingStatusMessage: ''
+};
+
+export default LoadingPageState;
