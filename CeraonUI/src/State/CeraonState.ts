002eff09517c6d775f1a49e4ea3a21bc891d97d1
Chris Botaish <chbota.dev@icloud.com>
2017-06-18 01:10:45 -0400

    Move ceraon initial state to the state folder

--- CeraonUI/src/State/CeraonState.ts
+++ CeraonUI/src/State/CeraonState.ts
@@ -1,9 +1,20 @@
-import UserSessionInfo from './Identity/UserSessionInfo';
-import NavigationBarState from './NavigationBarState';
+import UserSessionInfo, { DEFAULT_USER_SESSION_INFO } from './Identity/UserSessionInfo';
+import NavigationBarState, { DEFAULT_NAVIGATION_BAR_STATE } from './NavigationBarState';
+import LoadingPageState, { DEFAULT_LOADING_PAGE_STATE } from './LoadingPageState';
+import CeraonPage from './CeraonPage';
 
 interface CeraonState {
   userSessionInfo: UserSessionInfo;
   navigationBarState: NavigationBarState;
+  loadingPageState: LoadingPageState;
+  activePage: CeraonPage;
 }
+
+export const DEFAULT_CERAON_STATE: CeraonState = {
+  userSessionInfo: DEFAULT_USER_SESSION_INFO,
+  navigationBarState: DEFAULT_NAVIGATION_BAR_STATE,
+  loadingPageState: DEFAULT_LOADING_PAGE_STATE,
+  activePage: CeraonPage.Home,
+};
 
 export default CeraonState;
--- CeraonUI/src/Store/CeraonStore.ts
+++ CeraonUI/src/Store/CeraonStore.ts
@@ -1,13 +1,6 @@
 import * as Redux from 'redux';
-import CeraonState from '../State/CeraonState';
+import CeraonState, { DEFAULT_CERAON_STATE } from '../State/CeraonState';
 import CeraonReducer from './Reducers/CeraonReducer';
-import { DEFAULT_NAVIGATION_BAR_STATE } from '../State/NavigationBarState';
-import { DEFAULT_USER_SESSION_INFO } from '../State/Identity/UserSessionInfo';
 
-const initialState: CeraonState = {
-  userSessionInfo: DEFAULT_USER_SESSION_INFO,
-  navigationBarState: DEFAULT_NAVIGATION_BAR_STATE,
-};
-
-const ceraonStore = Redux.createStore(CeraonReducer, initialState);
+const ceraonStore = Redux.createStore(CeraonReducer, DEFAULT_CERAON_STATE);
 export default ceraonStore;
