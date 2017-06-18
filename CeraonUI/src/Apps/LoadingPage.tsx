77b116faee1f3d4bd0a0c3e9033678247c1ab6a3
Chris Botaish <chbota.dev@icloud.com>
2017-06-18 01:10:07 -0400

    Add loading page

---
+++ CeraonUI/src/Apps/LoadingPage.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+import { Loader, Dimmer } from 'semantic-ui-react';
+import LoadingPageState from '../State/LoadingPageState';
+
+export interface LoadingPageProps extends LoadingPageState, React.Props<LoadingPage> {
+  
+}
+
+export default class LoadingPage extends React.Component<LoadingPageProps, {}> {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+    <Dimmer active inverted>
+      <Loader active inline='centered' size='huge'>{this.props.loadingStatusMessage}</Loader>
+    </Dimmer>)
+  }
+
+}
