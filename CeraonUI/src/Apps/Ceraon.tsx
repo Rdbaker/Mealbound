aa8934f587491bc4f1446f741d599323f590ee67
Chris Botaish <chbota.dev@icloud.com>
2017-06-17 23:32:58 -0400

    Host app within AppHost

--- CeraonUI/src/Apps/Ceraon.tsx
+++ CeraonUI/src/Apps/Ceraon.tsx
@@ -1,26 +1,29 @@
 import * as React from 'react';
 
+import AppHost from '../Components/AppHost';
 import CeraonStore from '../Store/CeraonStore';
 import CeraonState from '../State/CeraonState';
 import NavigationBar from '../Components/NavigationBar';
 
 export default class Ceraon extends React.Component<{}, CeraonState> {
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		this.onStoreUpdate = this.onStoreUpdate.bind(this);
-		this.state = CeraonStore.getState();
-		CeraonStore.subscribe(this.onStoreUpdate);
-	}
+    this.onStoreUpdate = this.onStoreUpdate.bind(this);
+    this.state = CeraonStore.getState();
+    CeraonStore.subscribe(this.onStoreUpdate);
+  }
 
-	onStoreUpdate() {
-		this.setState(CeraonStore.getState());
-	}
+  onStoreUpdate() {
+    this.setState(CeraonStore.getState());
+  }
 
-	render() {
-		return (
-			<NavigationBar navigationBarState={this.state.navigationBarState} userSessionInfo={this.state.userSessionInfo}/>
-		)
-	}
+  render() {
+    return (
+      <AppHost>
+        <NavigationBar {...this.state.navigationBarState}/>
+      </AppHost>
+    )
+  }
 
 }
