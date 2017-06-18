44f9ba9843ff0cd9e93031abee329bab31c5d91a
Chris Botaish <chbota.dev@icloud.com>
2017-06-18 01:11:02 -0400

    Add support in reducer for login action

--- CeraonUI/src/Store/Reducers/CeraonReducer.ts
+++ CeraonUI/src/Store/Reducers/CeraonReducer.ts
@@ -1,6 +1,14 @@
 import CeraonState from '../../State/CeraonState';
 import CeraonAction from '../../Actions/CeraonAction';
+import CeraonActionType from '../../Actions/CeraonActionType';
+import CeraonPage from '../../State/CeraonPage';
 
 export default function ceraonReducer(state: CeraonState, action: CeraonAction) : CeraonState {
+
+  if (action.type == CeraonActionType.Login) {
+    state.activePage = CeraonPage.Loading;
+    state.loadingPageState.loadingStatusMessage = 'Logging you in...';
+  }
+
   return state;
 }
