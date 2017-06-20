f1dab9109fc1c29ab83f452a44243a9449521c22
Chris Botaish <chbota.dev@icloud.com>
2017-06-19 21:37:44 -0400

    Add home page impl

--- CeraonUI/src/Pages/HomePage.tsx
+++ CeraonUI/src/Pages/HomePage.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 
-import HomePageState from '../State/HomePageState';
+import {Segment, Header, Icon} from 'semantic-ui-react';
+import HomePageState from '../State/Pages/HomePageState';
+import MealSearchDropdown from '../Components/MealSearchDropdown';
+import { MealTime, MealTimeFilter } from '../State/Meal/Filters/MealTime';
+import CeraonDispatcher from '../Store/CeraonDispatcher';
+import CreateMealSearchAction, {MealSearchActionType} from '../Actions/MealSearchAction';
 
 export interface HomePageProps extends HomePageState, React.Props<HomePage> {
   
@@ -9,11 +14,23 @@ export interface HomePageProps extends HomePageState, React.Props<HomePage> {
 export default class HomePage extends React.Component<HomePageProps, {}> {
   constructor() {
     super();
+    this.onMealSearch = this.onMealSearch.bind(this);
+  }
+
+  onMealSearch(mealTime: MealTime) {
+    CeraonDispatcher(CreateMealSearchAction(MealSearchActionType.AddFilter,
+      [new MealTimeFilter(mealTime)]));
   }
 
   render() {
     return (
-    <div/>
+      <Header as='h2' icon textAlign='center'>
+        <Icon name='food' circular />
+        <Header.Content>
+          {this.props.headerMessage}
+          <MealSearchDropdown mealTimeOptions={this.props.searchMealTimeOptions} onSearchClicked={this.onMealSearch} />
+        </Header.Content>
+      </Header>
     )
   }
 }
--- CeraonUI/src/State/Pages/HomePageState.ts
+++ CeraonUI/src/State/Pages/HomePageState.ts
@@ -1,11 +1,16 @@
+import {MealTime} from '../Meal/Filters/MealTime';
+
+
 interface HomePageState {
   headerMessage: string;
   showMealSearchDropdown: boolean;
+  searchMealTimeOptions: MealTime[];
 }
 
 export const DEFAULT_HOME_PAGE_STATE: HomePageState = {
-  headerMessage: 'Find a place to eat nearby',
-  showMealSearchDropdown: true
+  headerMessage: 'Find a place nearby to eat',
+  showMealSearchDropdown: true,
+  searchMealTimeOptions: [MealTime.Any, MealTime.Breakfast, MealTime.Lunch, MealTime.Dinner],
 };
 
 export default HomePageState;
