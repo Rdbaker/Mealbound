2d1dc34f8b342f626a3cafbd5d6b2db01c247481
Chris Botaish <chbota.dev@icloud.com>
2017-06-19 23:19:44 -0400

    Add meal and location model

---
+++ CeraonUI/src/State/Meal/Location.ts
@@ -0,0 +1,12 @@
+
+interface Location {
+  name: string;
+  numDollarSigns: number;
+  rating: number;
+  latitude: number;
+  longitude: number;
+  address: string;
+  phone: string;
+}
+
+export default Location;
