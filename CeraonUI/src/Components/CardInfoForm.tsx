265bd631f86fcd6af2781a02c57284d79b3d0af1
Ryan Baker <ryan.da.baker@gmail.com>
2017-09-02 11:55:50 -0400

    Adds payment confirmation to the join meal flow.

--- CeraonUI/src/Components/CardInfoForm.tsx
+++ CeraonUI/src/Components/CardInfoForm.tsx
@@ -1,26 +1,73 @@
 import StripeCheckout from 'react-stripe-checkout';
+import { Button } from 'semantic-ui-react';
 import * as React from 'react';
 
 import * as Actions from '../Actions/Index';
 import CeraonDispatcher from '../Store/CeraonDispatcher';
 import CeraonModel from '../Services/CeraonModel';
 
+interface CardInfoFormProps {
+  stripeCheckoutName?: string;
+  triggerBtnColor?: string;
+  triggerBtnIcon?: string;
+  stripeCheckoutDescription: string;
+  stripeCheckoutPanelLabel: string;
+  stripeCheckoutLabel: string;
+  stripeCheckoutOnToken: (stripeToken: string) => void;
+  stripeSubmitBtnText: string;
+}
+
 export default class CardInfoForm extends React.Component<any, any> {
-  onToken(token) {
-    CeraonDispatcher(Actions.createUpdatePaymentInfoAction(token.id));
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: this.props.stripeCheckoutName || "Mealbound",
+      triggerBtnColor: this.props.triggerBtnColor || "violet",
+      triggerBtnIcon: this.props.triggerBtnIcon,
+      description: this.props.stripeCheckoutDescription,
+      panelLabel: this.props.stripeCheckoutPanelLabel,
+      label: this.props.stripeCheckoutLabel,
+      onToken: this.props.stripeCheckoutOnToken,
+      submitBtnText: this.props.stripeSubmitBtnText
+    };
+  }
+
+  hasTriggerBtnIcon() {
+    if (this.state.triggerBtnIcon) {
+      return "labeled icon";
+    } else {
+      return "";
+    }
+  }
+
+  getTriggerBtnIcon() {
+    if (this.state.triggerBtnIcon) {
+      return ( <i className={this.state.triggerBtnIcon}></i> );
+    } else {
+      return null;
+    }
   }
 
   render() {
     return (
       <StripeCheckout
-        name="Mealbound"
-        description="Update your payment info"
-        panelLabel="Update"
-        label="Update Payment Info"
-        token={this.onToken}
+        name={this.state.name}
+        description={this.state.description}
+        panelLabel={this.state.panelLabel}
+        label={this.state.label}
+        token={this.state.onToken}
         stripeKey={CeraonModel.getStripeKey()}
       >
-        <button className="ui labeled icon violet button"><i className="credit card icon"></i>Update Payment Info</button>
+        <Button
+         color={this.state.triggerBtnColor}
+         className={`${this.hasTriggerBtnIcon()}`}
+         disabled={this.props.triggerBtnDisabled}
+         loading={this.props.triggerBtnLoading}
+        >
+          {this.getTriggerBtnIcon()}
+          {this.state.submitBtnText}
+        </Button>
       </StripeCheckout>
     )
   }
--- CeraonUI/src/Services/Model/Tasks/ToggleJoinMealTask.ts
+++ CeraonUI/src/Services/Model/Tasks/ToggleJoinMealTask.ts
@@ -5,12 +5,12 @@ import Meal from '../../../State/Meal/Meal';
 import * as Actions from '../../../Actions/Index';
 
 export class ToggleJoinMealTask extends ModelTask<ModelUpdateResult<Meal>> {
-  constructor(private _api: ICeraonModelAPI, private _id: string, private _join: boolean, startLoading: boolean) {
+  constructor(private _api: ICeraonModelAPI, private _id: string, private _join: boolean, private _stripeToken: string, startLoading: boolean) {
     super(startLoading);
   }
 
   run(next?: (task: ModelTask<ModelUpdateResult<Meal>>, result: ModelUpdateResult<Meal>)=>void) {
-    this._api.toggleJoinMeal(this._id, this._join).then((result: ModelUpdateResult<Meal>) => {
+    this._api.toggleJoinMeal(this._id, this._join, this._stripeToken).then((result: ModelUpdateResult<Meal>) => {
       this.dispatchAction(Actions.createMealUpdatedAction(result));
       if (next) {
         next(this, result);
